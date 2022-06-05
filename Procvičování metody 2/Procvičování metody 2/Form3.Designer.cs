a90028c0556edc626b9a49d26abf9c8ef5693666
Antonín Štolfa <stolfaa.04@spst.eu>
2022-06-05 21:47:14 +0200

    Hotovo

--- Procvičování metody 2/Procvičování metody 2/Form3.Designer.cs
+++ Procvičování metody 2/Procvičování metody 2/Form3.Designer.cs
@@ -28,12 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.odpovedNSD = new System.Windows.Forms.Label();
+            this.odpovedNSN = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.LawnGreen;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(13, 132);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 224);
+            this.listBox1.TabIndex = 17;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.LawnGreen;
+            this.button1.Location = new System.Drawing.Point(66, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 48);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Zpracovat";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 31);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Zadejte dvě čísla:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown1.ForeColor = System.Drawing.Color.LawnGreen;
+            this.numericUpDown1.Location = new System.Drawing.Point(79, 43);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(54, 29);
+            this.numericUpDown1.TabIndex = 19;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown2.ForeColor = System.Drawing.Color.LawnGreen;
+            this.numericUpDown2.Location = new System.Drawing.Point(151, 43);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(54, 29);
+            this.numericUpDown2.TabIndex = 20;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.ForeColor = System.Drawing.Color.Gold;
+            this.button4.Location = new System.Drawing.Point(12, 409);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 29);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "Konec";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button4.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.Color.LawnGreen;
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(151, 132);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 224);
+            this.listBox2.TabIndex = 22;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label1.Location = new System.Drawing.Point(334, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 31);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "NSD:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label3.Location = new System.Drawing.Point(334, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 31);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "NSN:";
+            // 
+            // odpovedNSD
+            // 
+            this.odpovedNSD.AutoSize = true;
+            this.odpovedNSD.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.odpovedNSD.ForeColor = System.Drawing.Color.LawnGreen;
+            this.odpovedNSD.Location = new System.Drawing.Point(424, 132);
+            this.odpovedNSD.Name = "odpovedNSD";
+            this.odpovedNSD.Size = new System.Drawing.Size(30, 31);
+            this.odpovedNSD.TabIndex = 25;
+            this.odpovedNSD.Text = "1";
+            // 
+            // odpovedNSN
+            // 
+            this.odpovedNSN.AutoSize = true;
+            this.odpovedNSN.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.odpovedNSN.ForeColor = System.Drawing.Color.LawnGreen;
+            this.odpovedNSN.Location = new System.Drawing.Point(424, 177);
+            this.odpovedNSN.Name = "odpovedNSN";
+            this.odpovedNSN.Size = new System.Drawing.Size(30, 31);
+            this.odpovedNSN.TabIndex = 26;
+            this.odpovedNSN.Text = "1";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.odpovedNSN);
+            this.Controls.Add(this.odpovedNSD);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form3";
             this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label odpovedNSD;
+        private System.Windows.Forms.Label odpovedNSN;
     }
 }
--- Procvičování metody 2/Procvičování metody 2/Form3.cs
+++ Procvičování metody 2/Procvičování metody 2/Form3.cs
@@ -16,5 +16,83 @@ namespace Procvičování_metody_2
         {
             InitializeComponent();
         }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.ForeColor = Color.Gold;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.ForeColor = Color.LawnGreen;
+        }
+
+        private void button4_MouseEnter(object sender, EventArgs e)
+        {
+            button4.ForeColor = Color.Red;
+        }
+
+        private void button4_MouseLeave(object sender, EventArgs e)
+        {
+            button4.ForeColor = Color.Gold;
+        }
+        int NSD(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b) a -= b;
+                else b -= a;
+            }
+            return a;
+        }
+        private bool JePrvocislo(int cislo)
+        {
+            bool prvocislo = true;
+            if (cislo == 2) prvocislo = true;
+            else
+            {
+                if (cislo == 1 || cislo % 2 == 0) prvocislo = false;
+                else for (int delitel = 3; delitel <= Math.Sqrt(cislo) && prvocislo; ++delitel)
+                    {
+                        if (cislo % delitel == 0) return false;
+                    }
+            }
+            return prvocislo;
+        }
+        void Rozklad(int a, ListBox listbox)
+        {
+            int pomocna = 1;
+            listbox.Items.Clear();
+            int i = 2;
+            while (i <= a)
+            {
+                if (JePrvocislo(i) == false) ;
+                while (a % i == 0)
+                {
+                    listbox.Items.Add(i);
+                    a /= i;
+                }
+                if (i == 3) pomocna = 2;
+                i += pomocna;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = (int)numericUpDown1.Value;
+            int b = (int)numericUpDown2.Value;
+            Rozklad(a, listBox2);
+            Rozklad(b, listBox1);
+            odpovedNSD.Text = NSD(a, b).ToString();
+            odpovedNSN.Text = Convert.ToString((a * b) / NSD(a, b));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Menu menu = new Menu();
+            this.Hide();
+            menu.ShowDialog();
+            this.Close();
+        }
     }
 }
